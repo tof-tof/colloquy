cee0d9a5a72bc0cda239bff57775e7581da55c51
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2021-03-10 20:57:28 +0000

    colour themed the chat and formatted code

--- pages/chat/[secretkey]/[botName].js
+++ pages/chat/[secretkey]/[botName].js
@@ -5,110 +5,128 @@ import { useRouter } from "next/router";
 import React, { useRef, useState } from "react";
 import Message from "../../../components/Message";
 
-const BASE_URL = "https://www.colloquychatbotapi.com"
+const BASE_URL = "https://www.colloquychatbotapi.com";
 
 const Chat = () => {
-	const router = useRouter();
+  const router = useRouter();
 
-	const { secretkey, botName } = router.query;
+  const { secretkey, botName } = router.query;
 
-	const messagesEnd = useRef(null);
+  const messagesEnd = useRef(null);
 
-	const [messages, setMessages] = useState([
-		{
-			speaks: "bot",
-			msg: "hello there...!",
-		},
-	]);
+  const [messages, setMessages] = useState([
+    {
+      speaks: "bot",
+      msg: "hello there...!",
+    },
+  ]);
 
-	const df_text_query = async (queryText) => {
-		let says = {
-			speaks: "user",
-			msg: queryText,
-		};
+  const df_text_query = async (queryText) => {
+    let says = {
+      speaks: "user",
+      msg: queryText,
+    };
 
-		setMessages((prevMessages) => {
-			return [...prevMessages, says];
-		});
+    setMessages((prevMessages) => {
+      return [...prevMessages, says];
+    });
 
-		const res = await axios.get(
-			`${BASE_URL}/getResponse/secretkey=${secretkey}/modelName=${botName}/message=${queryText}`
-		);
+    const res = await axios.get(
+      `${BASE_URL}/getResponse/secretkey=${secretkey}/modelName=${botName}/message=${queryText}`
+    );
 
-		says = {
-			speaks: "bot",
-			msg: res.data.chat_response,
-		};
-		setMessages((prevMessages) => {
-			return [...prevMessages, says];
-		});
+    says = {
+      speaks: "bot",
+      msg: res.data.chat_response,
+    };
+    setMessages((prevMessages) => {
+      return [...prevMessages, says];
+    });
 
-		messagesEnd.current.focus();
+    messagesEnd.current.focus();
 
-		messagesEnd.current.scrollIntoView({ behavior: "smooth" });
-	};
+    messagesEnd.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-	const renderMessages = (returnedMessages) => {
-		if (returnedMessages) {
-			return returnedMessages.map((message, i) => {
-				return <Message key={i} speaks={message.speaks} text={message.msg} />;
-			});
-		} else {
-			return null;
-		}
-	};
+  const renderMessages = (returnedMessages) => {
+    if (returnedMessages) {
+      return returnedMessages.map((message, i) => {
+        return <Message key={i} speaks={message.speaks} text={message.msg} />;
+      });
+    } else {
+      return null;
+    }
+  };
 
-	const scroll = (ref) => {
-		ref.current.scrollIntoView({ behavior: "smooth" });
-	};
+  const scroll = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-	const _handleInputKeyPress = (e) => {
-		if (e.key === "Enter") {
-			df_text_query(e.target.value);
-			e.target.value = "";
-		}
-	};
+  const _handleInputKeyPress = (e) => {
+    if (e.key === "Enter") {
+      df_text_query(e.target.value);
+      e.target.value = "";
+    }
+  };
 
-	return (
-		<>
-			<Head>
-				<title>Chat Window - Colloquy</title>
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="manifest" href="/site.webmanifest"></link>
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>Chat Window - Colloquy</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
 
-			<div>
-				<div className="nav">
-					<h1 className="logo cursor-pointer" onClick={() => {
-						router.push("/app")
-					}}>Colloquy</h1>				
-				</div>
-				<div className="mx-auto w-min pt-24">
-					<img className="splash" src='/assets/flat_background.svg'></img>
-					<div
-						className="chat-window"
-					>
-						<div
-							id="chatBot"
-						>
-							{renderMessages(messages)}
-
-						</div>
-					</div>
-					<div className="input-container">
-						<input
-							ref={messagesEnd}
-							type="text"
-							placeholder="Say Hi..."
-							onKeyPress={_handleInputKeyPress}
-						/>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+      <div className="h-screen bg-gradient-to-br from-secondary via-transparent">
+        <div className="nav">
+          <h1
+            className="logo cursor-pointer text-7xl font-medium m-0"
+            onClick={() => {
+              router.push("/app");
+            }}
+          >
+            Colloquy
+          </h1>
+          <h3 className="text-2xl italic text-themeDarkBlue mt-4 ml-14">
+            Start the conversation
+          </h3>
+        </div>
+        <div className="ml-72 w-min pt-0">
+          <img
+            className="splash hidden xl:block"
+            src="/assets/flat_background.svg"
+          ></img>
+          <div className="chat-window">
+            <div id="chatBot">{renderMessages(messages)}</div>
+          </div>
+          <div className="input-container">
+            <input
+              ref={messagesEnd}
+              type="text"
+              placeholder="Say Hi..."
+              onKeyPress={_handleInputKeyPress}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Chat;
